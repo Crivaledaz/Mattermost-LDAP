ebe47e7daf86c78c438c1eebd64a29540e699247
Angus B. Grieve-Smith <grvsmth@panix.com>
2020-05-01 16:06:18 -0400

    Remove changes from other branches

--- oauth/config.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-$url_scheme = "https";
-
-?>
--- oauth/token.php
+++ oauth/token.php
@@ -6,27 +6,7 @@
 
 // include our OAuth2 Server object
 require_once __DIR__.'/server.php';
-require_once __DIR__.'/config.php';
 
-
-/*
-
-  The Mattermost server seems to be returning bare http urls, even if
-  there is no http url in the config.json.  If we are using https we
-  need to modify them.
-
-*/
-$redirect_url_scheme = substr($_POST["redirect_uri"], 0, 5);
-
-if ($url_scheme == "https" && $redirect_url_scheme == "http:") {
-    $_POST["redirect_uri"] = "https" . substr($_POST["redirect_uri"], 4);
-}
-
-/*
-
- Handle a request for an OAuth2.0 Access Token and send the response
- to the client
-
-*/
+// Handle a request for an OAuth2.0 Access Token and send the response to the client
 $server->handleTokenRequest(OAuth2\Request::createFromGlobals())->send();
 ?>
